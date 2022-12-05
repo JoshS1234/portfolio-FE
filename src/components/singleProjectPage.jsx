bbfff513f1a78f64624502ee035e6bc4f3bb00cd
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2022-12-05 23:09:48 +0000

    updated headerbar (temporary SVG image)

--- src/components/singleProjectPage.jsx
+++ src/components/singleProjectPage.jsx
@@ -6,9 +6,11 @@ import { Link } from "react-router-dom";
 
 const SingleProjectPage = () => {
   let url = window.location.href;
-  let project_id = url.split(
-    "https://portfolio-joshua-spence.netlify.app/projects/"
-  )[1];
+  // let project_id = url.split(
+  //   "https://portfolio-joshua-spence.netlify.app/projects/"
+  // )[1];
+
+  let project_id = url.split("localhost:3000/projects/")[1];
 
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -44,54 +46,58 @@ const SingleProjectPage = () => {
         <Link className="navLink" to={`/projects`}>
           <button>Back to projects</button>
         </Link>
-        <h3>Project Title: {project.name}</h3>
-        <h5>Programming Language: {project.language}</h5>
-        {project.image_url === "" ? (
-          project.video_url ? (
-            <YoutubeEmbed embedId={project.video_url} />
-          ) : (
-            <p></p>
-          )
-        ) : (
-          <img
-            className="projectImage"
-            src={require(`./Images/${project.project_id}.png`)}
-            alt="demonstration of the functionality of this project"
-          />
-        )}
-        {/* {project.video_url === "" ? (
-          <p></p>
-        ) : (
-          <YoutubeEmbed embedId={project.video_url} />
-        )} */}
-        {project.tech === "" ? (
-          <p></p>
-        ) : (
-          <h5>Additional tech: {project.tech}</h5>
-        )}
-        <h5>Description: {project.description}</h5>
+        <div>
+          <h3>Project Title: {project.name}</h3>
+          <div className="detailsAndImage">
+            <div className="imageOrVideo">
+              {project.image_url === "" ? (
+                <></>
+              ) : (
+                <img
+                  className="projectImage"
+                  src={require(`./Images/${project.project_id}.png`)}
+                  alt="demonstration of the functionality of this project"
+                />
+              )}
+              {project.video_url === "" ? (
+                <></>
+              ) : (
+                <YoutubeEmbed embedId={project.video_url} />
+              )}
+            </div>
+            <div className="details">
+              <h5>Programming Language: {project.language}</h5>
+              {project.tech === "" ? (
+                <p></p>
+              ) : (
+                <h5>Additional tech: {project.tech}</h5>
+              )}
+              <h5>Description: {project.description}</h5>
 
-        {project.github === "" ? (
-          <h5>Github: N/A</h5>
-        ) : (
-          <h5>
-            Github:
-            <a target="_blank" rel="noreferrer" href={project.github}>
-              {project.github}
-            </a>
-          </h5>
-        )}
-        {project.hosted === "" ? (
-          <h5>Hosted Site: N/A</h5>
-        ) : (
-          <h5>
-            Hosted Site:{" "}
-            <a target="_blank" rel="noreferrer" href={project.github}>
-              {project.hosted}
-            </a>
-          </h5>
-        )}
-        <h5>Year: {project.year}</h5>
+              {project.github === "" ? (
+                <h5>Github: N/A</h5>
+              ) : (
+                <h5>
+                  Github:
+                  <a target="_blank" rel="noreferrer" href={project.github}>
+                    {project.github}
+                  </a>
+                </h5>
+              )}
+              {project.hosted === "" ? (
+                <h5>Hosted Site: N/A</h5>
+              ) : (
+                <h5>
+                  Hosted Site:{" "}
+                  <a target="_blank" rel="noreferrer" href={project.github}>
+                    {project.hosted}
+                  </a>
+                </h5>
+              )}
+              <h5>Year: {project.year}</h5>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
